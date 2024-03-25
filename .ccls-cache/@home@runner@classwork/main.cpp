126bc97ac3e12f2e8cbd99f4d11a2850c8aa567d
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-25 01:36:46 +0000

    work

--- .ccls-cache/@home@runner@classwork/main.cpp
+++ .ccls-cache/@home@runner@classwork/main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+g#include <iostream>
 
 int main() {
   std::cout << "Hello World!\n";
+
+  // test Use a for loop to calculate the factorial of an integer.
 }
--- main.cpp
+++ main.cpp
@@ -2,4 +2,6 @@ g#include <iostream>
 
 int main() {
   std::cout << "Hello World!\n";
+
+  // test Use a for loop to calculate the factorial of an integer.
 }
